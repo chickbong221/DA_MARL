62e82c66603d758114746b97983371b608063d68
Le Tuan <letuanhf@gmail.com>
2024-07-18 05:43:24 +0700

    update

--- DA-MARL.py
+++ DA-MARL.py
@@ -3,73 +3,52 @@ import random
 from copy import copy
 
 import numpy as np
-from gymnasium.spaces import Discrete, MultiDiscrete
+from gymnasium.spaces import Box
 
 from pettingzoo import ParallelEnv
 
 
 class CustomEnvironment(ParallelEnv):
-    """The metadata holds environment constants.
-
-    The "name" metadata allows the environment to be pretty printed.
-    """
 
     metadata = {
         "name": "custom_environment",
     }
 
     def __init__(self):
-        """The init method takes in environment arguments.
+       
+        #agent_list
+        self.possible_agents = [f"consumer_{i+1}" for i in range(4)] + [f"prosumer_{i+1}" for i in range(4)]
 
-        Should define the following attributes:
-        - escape x and y coordinates
-        - guard x and y coordinates
-        - prisoner x and y coordinates
-        - timestamp
-        - possible_agents
-
-        Note: as of v1.18.1, the action_spaces and observation_spaces attributes are deprecated.
-        Spaces should be defined in the action_space() and observation_space() methods.
-        If these methods are not overridden, spaces will be inferred from self.observation_spaces/action_spaces, raising a warning.
-
-        These attributes should not be changed after initialization.
-        """
+        #grid price
         self.FiT = 0.04
         self.ToU = np.zeros((24)) + 0.08; self.ToU[9:17] = 0.13; self.ToU[17:21] = 0.18
-        self.
-        self.possible_agents = ["consumer" for i in range(4)] + ["prosumer" for i in range(4)]
+
+        #ES properties
+        self.ES_maximum_energy_level = 10
+        self.ES_minimum_energy_level = 2
+        self.ES_power_capacity = 2
+        self.ES_charging_efficiency = 0.95
+        self.ES_discharging_efficiency = 0.95
+        self.ES_energy_level = {a: np.clip(np.random.normal(6,1), 2, 10) for a in self.possible_agents}
 
     def reset(self, seed=None, options=None):
-        """Reset set the environment to a starting point.
 
-        It needs to initialize the following attributes:
-        - agents
-        - timestamp
-        - prisoner x and y coordinates
-        - guard x and y coordinates
-        - escape x and y coordinates
-        - observation
-        - infos
-
-        And must set up the environment so that render(), step(), and observe() can be called without issues.
-        """
+        if seed is not None:
+            np.random.seed(seed)
+        
         self.agents = copy(self.possible_agents)
         self.timestep = 0
 
-        self.prisoner_x = 0
-        self.prisoner_y = 0
-
-        self.guard_x = 6
-        self.guard_y = 6
-
-        self.escape_x = random.randint(2, 5)
-        self.escape_y = random.randint(2, 5)
+        # self.ES_energy_level = {a: np.clip(np.random.normal(6,1), 2, 10) for a in self.agents}
+        self.inflexible_demand = {a: min(0, np.random.normal(7, 3)) for a in self.agents}
+        self.PV_generation = {a: 0 if "consumer" in a else np.random.normal(12, 3) for a in self.agents}
 
         observations = {
             a: (
-                self.prisoner_x + 7 * self.prisoner_y,
-                self.guard_x + 7 * self.guard_y,
-                self.escape_x + 7 * self.escape_y,
+                self.inflexible_demand[a] - self.PV_generation[a],
+                self.ES_energy_level[a],
+                self.FiT,
+                self.ToU[self.timestep],
             )
             for a in self.agents
         }
@@ -80,65 +59,112 @@ class CustomEnvironment(ParallelEnv):
         return observations, infos
 
     def step(self, actions):
-        """Takes in an action for the current agent (specified by agent_selection).
 
-        Needs to update:
-        - prisoner x and y coordinates
-        - guard x and y coordinates
-        - terminations
-        - truncations
-        - rewards
-        - timestamp
-        - infos
+        self.shared_reward = 0
+        self.timestep += 1
+        self.inflexible_demand = {a: min(0, np.random.normal(6, 2)) for a in self.agents}
+        self.PV_generation = {a: 0 if "consumer" in a else np.random.normal(11, 2) for a in self.agents}
 
-        And any internal state used by observe() or render()
-        """
-        # Execute actions
-        prisoner_action = actions["prisoner"]
-        guard_action = actions["guard"]
+        # Execute actions and get trading information
+        buyer_set = []
+        seller_set = []
+        for agent in self.agents:
+            
+            # Calculate quantity for trading
+            inflexible_load = self.inflexible_demand[agent] - self.PV_generation[agent]
+            current_ES_energy_level = self.ES_energy_level[agent]
+            quantity_for_ES = actions[agent][0]
+            # print(quantity_for_ES) #debug
 
-        if prisoner_action == 0 and self.prisoner_x > 0:
-            self.prisoner_x -= 1
-        elif prisoner_action == 1 and self.prisoner_x < 6:
-            self.prisoner_x += 1
-        elif prisoner_action == 2 and self.prisoner_y > 0:
-            self.prisoner_y -= 1
-        elif prisoner_action == 3 and self.prisoner_y < 6:
-            self.prisoner_y += 1
+            if quantity_for_ES >= 0:
+                C = min(quantity_for_ES * self.ES_power_capacity, (self.ES_maximum_energy_level - current_ES_energy_level) / self.ES_charging_efficiency)
+                D = 0
+            if quantity_for_ES < 0:
+                C = 0
+                D = min(quantity_for_ES*self.ES_power_capacity, (self.ES_minimum_energy_level - current_ES_energy_level) * self.ES_discharging_efficiency)
+            quantity_final = inflexible_load + C + D
+            self.ES_energy_level[agent] += (C*self.ES_charging_efficiency + D*self.ES_discharging_efficiency)
 
-        if guard_action == 0 and self.guard_x > 0:
-            self.guard_x -= 1
-        elif guard_action == 1 and self.guard_x < 6:
-            self.guard_x += 1
-        elif guard_action == 2 and self.guard_y > 0:
-            self.guard_y -= 1
-        elif guard_action == 3 and self.guard_y < 6:
-            self.guard_y += 1
+
+            # Calculate price for trading
+            price_selection = actions[agent][1]
+            # print(price_selection)
+
+            price_final = self.FiT + price_selection * (self.ToU[self.timestep] - self.FiT) 
+            # print(price_final)
+
+            # Apply abs() before go in DA algorithm
+            if quantity_final >= 0:
+                buyer_set.append([agent, np.abs(quantity_final), price_final])
+            else:
+                seller_set.append([agent, np.abs(quantity_final), price_final])
+            print(buyer_set)
+
+        #Sort order book
+        buyer_set_sorted = sorted(buyer_set, key=lambda a: a[2], reverse=True)
+        seller_set_sorted = sorted(seller_set, key=lambda a: a[2], reverse=False)
+        # a = [u.shape for u in seller_set_sorted]
+        # print(a)
+
+        #Double Auction
+        trading_history = {a: [] for a in self.agents}
+        i = 0
+        j = 0
+        # print(buyer_set_sorted[i][2], seller_set_sorted[j][2] )
+        while buyer_set_sorted[i][2] > seller_set_sorted[j][2]:
+            q_trade = min(buyer_set_sorted[i][1], seller_set_sorted[j][1])
+            p_trade = (buyer_set_sorted[i][2] + seller_set_sorted[j][2])/2
+
+            if buyer_set_sorted[i][1] <= q_trade:
+                buyer_set_sorted[i][1] = 0
+                seller_set_sorted[j][1] -= q_trade
+
+                trading_history[buyer_set_sorted[i][0]].append([q_trade, p_trade])
+                trading_history[seller_set_sorted[j][0]].append([-q_trade, p_trade])
+                i += 1
+
+            if seller_set_sorted[j][1] <= q_trade:
+                seller_set_sorted[j][1] = 0
+                buyer_set_sorted[i][1] -= q_trade
+
+                trading_history[buyer_set_sorted[i][0]].append([q_trade, p_trade])
+                trading_history[seller_set_sorted[j][0]].append([-q_trade, p_trade])
+                j += 1
+
+            if i > (len(buyer_set_sorted) - 1) or j > (len(seller_set_sorted) - 1):
+                break
+
+        for i in range(len(buyer_set_sorted)):
+            if buyer_set_sorted[i][1] != 0:
+                trading_history[buyer_set_sorted[i][0]].append([buyer_set_sorted[i][1], self.ToU[self.timestep]]) 
+                buyer_set_sorted[i][1] = 0
+        for j in range(len(seller_set_sorted)):
+            if seller_set_sorted[j][1] != 0:
+                trading_history[seller_set_sorted[j][0]].append([-seller_set_sorted[j][1], self.FiT])
+                seller_set_sorted[j][1] = 0
+
+        for agent in self.agents:
+            for k in range(len(trading_history[agent])):
+                self.shared_reward += trading_history[agent][k][0]*trading_history[agent][k][1]
+        self.shared_reward /= len(self.agents)
+
+        rewards = {a: self.shared_reward for a in self.agents}
 
         # Check termination conditions
         terminations = {a: False for a in self.agents}
-        rewards = {a: 0 for a in self.agents}
-        if self.prisoner_x == self.guard_x and self.prisoner_y == self.guard_y:
-            rewards = {"prisoner": -1, "guard": 1}
-            terminations = {a: True for a in self.agents}
-
-        elif self.prisoner_x == self.escape_x and self.prisoner_y == self.escape_y:
-            rewards = {"prisoner": 1, "guard": -1}
-            terminations = {a: True for a in self.agents}
 
         # Check truncation conditions (overwrites termination conditions)
         truncations = {a: False for a in self.agents}
-        if self.timestep > 100:
-            rewards = {"prisoner": 0, "guard": 0}
-            truncations = {"prisoner": True, "guard": True}
-        self.timestep += 1
-
+        if self.timestep == 23:
+            truncations = {a: True for a in self.agents}
+        
         # Get observations
         observations = {
             a: (
-                self.prisoner_x + 7 * self.prisoner_y,
-                self.guard_x + 7 * self.guard_y,
-                self.escape_x + 7 * self.escape_y,
+                self.inflexible_demand[a] - self.PV_generation[a],
+                self.ES_energy_level[a],
+                self.FiT,
+                self.ToU[self.timestep],
             )
             for a in self.agents
         }
@@ -149,26 +175,28 @@ class CustomEnvironment(ParallelEnv):
         if any(terminations.values()) or all(truncations.values()):
             self.agents = []
 
+        self.timestep += 1
+
         return observations, rewards, terminations, truncations, infos
 
     def render(self):
         """Renders the environment."""
-        grid = np.full((7, 7), " ")
-        grid[self.prisoner_y, self.prisoner_x] = "P"
-        grid[self.guard_y, self.guard_x] = "G"
-        grid[self.escape_y, self.escape_x] = "E"
-        print(f"{grid} \n")
-
-    # Observation space should be defined here.
-    # lru_cache allows observation and action spaces to be memoized, reducing clock cycles required to get each agent's space.
-    # If your spaces change over time, remove this line (disable caching).
+        
+    #observation space
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
-        # gymnasium spaces are defined and documented here: https://gymnasium.farama.org/api/spaces/
-        return MultiDiscrete([7 * 7] * 3)
+        return Box(low=-np.inf, high=np.inf, shape=(4,), dtype=np.float32)
 
-    # Action space should be defined here.
-    # If your spaces change over time, remove this line (disable caching).
+    #action space
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
-        return Discrete(4)
+        low = np.array([-1, 0])
+        high = np.array([1, 1])
+        return Box(low=low, high=high, dtype=np.float32)
+    
+
+from pettingzoo.test import parallel_api_test
+
+if __name__ == "__main__":
+    env = CustomEnvironment()
+    parallel_api_test(env, num_cycles=1_000_000)
--- test.py
+++ test.py
@@ -3,6 +3,52 @@ import numpy as np
 # x = [[3, 5], [1, 9], [4, 7], [2, 8]]
 # print(np.zeros((24))+0.1)
 
-x = ["ahihi" for i in range(3)] + ["ga" for i in range(2)]
-infos = {a: {} for a in x}
-print(infos)
+# possible_agents = [f"consumer_{i+1}" for i in range(4)] + [f"prosumer_{i+1}" for i in range(4)]
+# ES_begin = {
+#     a: np.clip(np.random.normal(6,1), 2, 10) for a in possible_agents
+# }
+# print(ES_begin)
+
+# def return_test():
+#     return "ahihi"
+
+# # for i in range(3):
+# #     print(return_test())
+
+# f = 1
+
+# def big_func():
+#     # f = 0
+#     while f < 5:
+#         f += 1
+#         print(f"This is {f-1}")
+#         print(return_test())
+
+# big_func() 
+# possible_agents = [f"consumer_{i+1}" for i in range(4)] + [f"prosumer_{i+1}" for i in range(4)]
+
+# PV_generation = {
+#     a: 0 if "consumer" in a else np.random.normal(15, 3) for a in possible_agents
+# }
+# for a in possible_agents:
+#     print(PV_generation[a])
+
+# possible_agents = [f"consumer_{i+1}" for i in range(4)] + [f"prosumer_{i+1}" for i in range(4)]
+
+# for agent in possible_agents:
+#     ahihi = {a: (agent, 0.1, 0.9) for a in range(3)}
+# print(ahihi[1][0])
+
+# a = { 
+#     "cam": []
+#     }
+# for i in range(3):
+#     a["cam"].append(i)
+# print(a["cam"])
+
+# a = [[1,2],[2,4],[6,3]]
+# buyer_set_sorted = sorted(a, key=lambda a: a[1], reverse=True)
+# print(buyer_set_sorted)
+
+a=10
+print(-a)
